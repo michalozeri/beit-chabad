b9a0e4860ee96e29b94a9f09b78e6e0122ce7c87
Michal Ozeri <michalozeri88@gmail.com>
2022-06-06 14:52:02 +0300

    search input

--- src/cmps/Filter.jsx
+++ src/cmps/Filter.jsx
@@ -1,10 +1,18 @@
-
-export function Filter({onFilterInput, filterBy}){
-
-  
-    return (
-        <section className="filter">
-            <input className="filter-input" placeholder="חיפוש לפי שם" type="text" name="fullname" value={filterBy} id="" onChange={onFilterInput} />
-        </section>
-    )
+export function Filter({ onFilterInput, filterBy }) {
+  return (
+    <section className="filter">
+      <div className="filter-container">
+        <img src={require("../assets/imgs/search.png")} alt="" />
+        <input
+          className="filter-input"
+          placeholder="חיפוש לפי שם"
+          type="text"
+          name="fullname"
+          value={filterBy}
+          id=""
+          onChange={onFilterInput}
+        />
+      </div>
+    </section>
+  );
 }
