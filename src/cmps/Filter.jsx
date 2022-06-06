128e0ebc1ef57e68bf3d9814225e7ad84564e277
Michal Ozeri <michalozeri88@gmail.com>
2022-06-06 12:03:15 +0300

    inputcenter

--- src/cmps/Filter.jsx
+++ src/cmps/Filter.jsx
@@ -4,7 +4,7 @@ export function Filter({onFilterInput, filterBy}){
   
     return (
         <section className="filter">
-            <input type="text" name="fullname" value={filterBy} id="" onChange={onFilterInput} />
+            <input className="filter-input" placeholder="חיפוש לפי שם" type="text" name="fullname" value={filterBy} id="" onChange={onFilterInput} />
         </section>
     )
 }
