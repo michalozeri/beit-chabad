c9f522bab0296751afb6c2a1bf299c9bc247e17c
Michal Ozeri <michalozeri88@gmail.com>
2022-06-27 11:33:12 +0300

    split line

--- src/cmps/DonatorPrev.jsx
+++ src/cmps/DonatorPrev.jsx
@@ -8,6 +8,7 @@ export function DonatorPrev({donator}) {
     <Paper style={{display: "flex", flexDirection:"column", justifyContent: "center", alignItems: "center",margin:"auto", border: "none", backgroundColor:"transparent",height:"70vh"}}>
         <div style={{fontSize: donator.isTooLong? "2.5rem": "4rem", width: "65vw", textAlign: "center", margin: "auto"}}>
           <p >{donator?.txt1}</p>
+          <p >{donator?.txt11}</p>
           <p style={{marginTop: donator.txt1? "2%": "0", fontWeight: "bold", fontSize: donator.isTooLong? "2rem": donator.txt1? "3.2rem": "4rem"}}>{donator.txt2}</p>
         </div>
     </Paper>  
